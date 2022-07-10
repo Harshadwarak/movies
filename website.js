fca4323f4e6c78266bcde8713910793b35ce240a
Harshadwarak <105093962+Harshadwarak@users.noreply.github.com>
2022-07-10 13:41:46 +0530

    added initial movies website files

---
+++ website.js
@@ -0,0 +1,10 @@
+function mssg()
+{
+alert("this is a Private website");
+}
+window.onload=mssg();
+
+//var temp=document.getElementsByClassName("header");
+//temp.style.fontSize="30px";
+//console.log(temp);
+document.getElementsByClassName('header').style.fontSize="100px";
